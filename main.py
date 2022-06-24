f496b9617955006ccedcf1918e0082303adee61b
RomashinD <https://github.com/RomashinD/bellhard_52.git>
2022-06-24 20:59:44 +0300

    innitial commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 count = 0
 while count == 0:
   word = input()
-    if word == 'hello':
-      count +=1
-print('hello')
+  if word != 'hello':\
+    print('wrong')
+  if word == 'hello':
+    count +=1
+import this
