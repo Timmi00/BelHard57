fa85d35d1222bcd5cb71b2c1e193fe73a6298ce1
timmi00 <TRIm200988>
2022-07-04 16:02:18 +0300

    Change name

--- Lesson_4/4.2.py
+++ Lesson_4/4.2.py
@@ -2,13 +2,13 @@
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
 import re
-Some_list = str((input().upper()))
+User_list = str((input().upper()))
 # Dict_for_letters = {}
 # for n in Some_list:
 #     if n not in Dict_for_letters:
 #         Dict_for_letters[n] = 1
 #     else:
 #         Dict_for_letters[n] += 1
-Count: int = len(Some_list)
-Dict_for_letters = {Some_list[i]: len(re.findall(Some_list[i], Some_list)) for i in range(0, Count)}
+Count: int = len(User_list)
+Dict_for_letters = {User_list[i]: len(re.findall(User_list[i], User_list)) for i in range(0, Count)}
 print(Dict_for_letters.items())
