4b43b678b797daf0e0d0c8b57b37929e5f8012d3
Timmi00 <TIm200988>
2022-07-06 14:16:39 +0300

    add dict comprehenshion

--- Lesson_4/4.2.py
+++ Lesson_4/4.2.py
@@ -2,13 +2,12 @@
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
 import re
-User_list = str((input().upper()))
+User_string = str((input().upper()))
 # Dict_for_letters = {}
 # for n in Some_list:
 #     if n not in Dict_for_letters:
 #         Dict_for_letters[n] = 1
 #     else:
 #         Dict_for_letters[n] += 1
-Count: int = len(User_list)
-Dict_for_letters = {User_list[i]: len(re.findall(User_list[i], User_list)) for i in range(0, Count)}
-print(Dict_for_letters.items())
+print({User_list[i]: User_list.count(User_list[i]) for i in range(0, len(User_list))})
+
--- Lesson_4/4.3.py
+++ Lesson_4/4.3.py
@@ -5,5 +5,4 @@ List_name_email: [str] = str(input()).split(' ')
 Name: list[str] = List_name_email[:: 2]
 Email: list[str] = List_name_email[1::2]
 Count: int = round(len(List_name_email)/2)
-Dict_result = {i: {Name[i]: Email[i]}.popitem() for i in range(0, Count)}
-print(Dict_result)
+print({i: {Name[i]: Email[i]}.popitem() for i in range(0, Count)})
