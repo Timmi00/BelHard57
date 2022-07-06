2a1c0c1c340f02bc2fbed3dc65791e5f4e8a79dc
Timmi00 <TIm200988>
2022-07-06 16:26:02 +0300

    new solution

--- Lesson_4/4.1.py
+++ Lesson_4/4.1.py
@@ -1,3 +1,3 @@
 #Заполнить список степенями числа 2 (от 2^1 до 2^n)
-N_count = int(input())
-print([2**i for i in range(1, N_count+1)])
+n_count = int(input())
+print([2**i for i in range(1, n_count+1)])
