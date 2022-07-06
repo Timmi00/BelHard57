61b1b9f2a46c6557bd9d48184c1acd892c7b5cde
Timmi00 <TIm200988>
2022-07-06 16:27:20 +0300

    new solution

--- Lesson_4/4.3.py
+++ Lesson_4/4.3.py
@@ -4,4 +4,4 @@
 # Name_email = (input()).split(' ')
 # Count_words: int = len(Name_email)
 # print({round(i/2): {'Name': Name_email[i], 'Email': Name_email[i+1]} for i in range(0, Count_words, 2)})
-print({round(i): {'Name': input(), 'Email': input()} for i in range(0, int(input()))})
+print({i: {'Name': input(), 'Email': input()} for i in range(0, int(input()))})
