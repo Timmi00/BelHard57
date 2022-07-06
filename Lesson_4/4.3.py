d2d1517de2a6a13268c958083bd527feaec71d9a
Timmi00 <TIm200988>
2022-07-06 12:49:54 +0300

    add dict comprehenshion

--- Lesson_4/4.3.py
+++ Lesson_4/4.3.py
@@ -1,16 +1,9 @@
 # *Заполнить словарь где ключами будут выступать числа от 0 до n, а
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
-Name_email: [str] = str(input()).split(' ')
-Name: list[str] = Name_email[:: 2]
-Email: list[str] = Name_email[1::2]
-Count: int = round(len(Name_email)/2)
-# Dict_name_email = {}
-# for i in range(0, len(Name)):
-#     Dict_name_email[Name[i]] = Email[i]
-# Dict_result = {}
-# for n in range(0, len(Name)):
-#     Dict_result[n] = Dict_name_email.popitem()
-Dict_name_email = {Name[i]: Email[i] for i in range(0, Count)}
-Dict_result = {i: Dict_name_email.popitem() for i in range(0, Count)}
+List_name_email: [str] = str(input()).split(' ')
+Name: list[str] = List_name_email[:: 2]
+Email: list[str] = List_name_email[1::2]
+Count: int = round(len(List_name_email)/2)
+Dict_result = {i: {Name[i]: Email[i]}.popitem() for i in range(0, Count)}
 print(Dict_result)
