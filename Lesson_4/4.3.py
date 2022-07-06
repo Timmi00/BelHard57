7f7495eb170b072e0119e083f6521290f8818e6b
Timmi00 <TIm200988>
2022-07-06 16:26:39 +0300

    new solution

--- Lesson_4/4.3.py
+++ Lesson_4/4.3.py
@@ -1,4 +1,7 @@
 # *Заполнить словарь где ключами будут выступать числа от 0 до n, а
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
+# Name_email = (input()).split(' ')
+# Count_words: int = len(Name_email)
+# print({round(i/2): {'Name': Name_email[i], 'Email': Name_email[i+1]} for i in range(0, Count_words, 2)})
 print({round(i): {'Name': input(), 'Email': input()} for i in range(0, int(input()))})
