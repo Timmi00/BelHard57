9a794b31dfe8b567b189df8c610b7efe0374dcb9
Timmi00 <TIm200988>
2022-07-06 16:25:47 +0300

    new solution

--- Lesson_4/4.2.py
+++ Lesson_4/4.2.py
@@ -2,6 +2,6 @@
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
 
-User_string = str((input().upper()))
-print({User_string[i]: User_string.count(User_string[i]) for i in range(0, len(User_string))})
+user_string = str((input().upper()))
+print({user_string[i]: user_string.count(user_string[i]) for i in range(0, len(user_string))})
 
