cf9a86caa984a34c49769699242abbd8f60631a6
Timmi00 <TIm200988>
2022-07-06 16:14:49 +0300

    new solution

--- Lesson_4/4.2.py
+++ Lesson_4/4.2.py
@@ -1,13 +1,7 @@
 # Без использования collections, написать программу, которая будет
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
-# import re
+
 User_string = str((input().upper()))
-# Dict_for_letters = {}
-# for n in Some_list:
-#     if n not in Dict_for_letters:
-#         Dict_for_letters[n] = 1
-#     else:
-#         Dict_for_letters[n] += 1
 print({User_string[i]: User_string.count(User_string[i]) for i in range(0, len(User_string))})
 
