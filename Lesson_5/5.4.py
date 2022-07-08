0a343b10b6afdca18fe01bca7e6cd74f2d6ebc89
Timmi00 <TIm200988>
2022-07-08 11:01:48 +0300

    Add new lite solution

--- Lesson_5/5.4.py
+++ Lesson_5/5.4.py
@@ -5,10 +5,6 @@ i: int = 0
 while i < len(user_string) - 1:
     if user_string[i] == user_string[i + 1]:
         count_non_intersecting += 1
-        if user_string[i + 1] == user_string[i + 2]:
-            i += 2
-        else:
-            i += 1
-    else:
         i += 1
+    i += 1
 print('Общее количество повторений: ', count_non_intersecting)
