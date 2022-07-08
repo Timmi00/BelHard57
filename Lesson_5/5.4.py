30a250493af542de1e4db841dad18755e75597bd
Timmi00 <TIm200988>
2022-07-08 10:42:02 +0300

    Add new lite solution

--- Lesson_5/5.4.py
+++ Lesson_5/5.4.py
@@ -1,9 +1,14 @@
 # Вводится строка, определить количество пар одинаковых символов рядом стоящих в строке (не пересекающиеся вхождения)
 user_string = input('Введите строку:')
-res_dict: dict = {}
-res_str: str = ''
-for i in range(len(user_string) - 1):
+count_non_intersecting: int = 0
+i: int = 0
+while i < len(user_string) - 1:
     if user_string[i] == user_string[i + 1]:
-        res_str = user_string[i] + user_string[i+1]
-        res_dict.setdefault(res_str, user_string.count(res_str))
-print('Общее количество повторений: ', sum(res_dict.values()))
+        count_non_intersecting += 1
+        if user_string[i + 1] == user_string[i + 2]:
+            i += 2
+        else:
+            i += 1
+    else:
+        i += 1
+print('Общее количество повторений: ', count_non_intersecting)
