5c33decd86e6af27ed26a5eaa1da0d8d7ffe59f8
timmi00 <TRIm200988>
2022-07-10 17:55:13 +0300

    Add solution

---
+++ Lesson_6/6.7.py
@@ -0,0 +1,12 @@
+# Дан список чисел, необходимо для каждого элемента посчитать сумму его
+# соседей, для крайних чисел одним из соседей является число с противоположной
+# стороны списка
+list_numbers: list[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+list_numbers.insert(0, list_numbers[len(list_numbers) - 1])
+list_numbers.append(list_numbers[1])
+count = 1
+sum_result: list = []
+while count < len(list_numbers) - 1:
+    sum_result.append([list_numbers[count], (list_numbers[count - 1] + list_numbers[count + 1])])
+    count += 1
+print(sum_result)
