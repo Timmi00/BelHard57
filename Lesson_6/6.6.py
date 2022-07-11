f6526b44185b89b1d12d2c6d51a943a1f75c18b0
Timmi00 <TIm200988>
2022-07-11 14:15:56 +0300

    Add solution

--- Lesson_6/6.6.py
+++ Lesson_6/6.6.py
@@ -3,8 +3,8 @@
 from random import randint
 list_numbers: list[int] = [randint(1, 10000) for i in range(10)]
 print(list_numbers)
-for count in range(0, len(list_numbers)):
-    if list_numbers[count] % 2 == 0:
-        list_numbers.insert(0, list_numbers.pop(count))
-    count -= 1
+for i in range(0, len(list_numbers)):
+    if list_numbers[i] % 2 == 0:
+        list_numbers.insert(0, list_numbers.pop(i))
+    i -= 1
 print(list_numbers)
