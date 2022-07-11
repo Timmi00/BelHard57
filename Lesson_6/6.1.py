e11021c104df0748f2ec4a4472b18a87c6bdfe7a
Timmi00 <TIm200988>
2022-07-11 14:20:58 +0300

    Add solution

--- Lesson_6/6.1.py
+++ Lesson_6/6.1.py
@@ -9,9 +9,9 @@ def dec_bin_dec(num):
     while num:
         number_bin = str(num % 2) + number_bin
         num = num // 2
-    for i in range(0, len(number_bin)):
-        number_dec = number_dec + int(number_bin[i]) * (2 ** (len(number_bin) - i - 1))
-    return number_bin, number_dec
+    # for i in range(0, len(number_bin)):
+    #     number_dec = number_dec + int(number_bin[i]) * (2 ** (len(number_bin) - i - 1))
+    return number_bin
 
 
-print(dec_bin_dec(user_number))
+print(dec_bin_dec(user_number), user_number)
