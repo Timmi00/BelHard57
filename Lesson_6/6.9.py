8ea8c5d161eee30d0e0aba556b1cdaae8c67ab20
Timmi00 <TIm200988>
2022-07-11 10:51:56 +0300

    Add new lite solution

--- Lesson_6/6.9.py
+++ Lesson_6/6.9.py
@@ -2,3 +2,16 @@
 # словарь с данными о пользователе (имя, фамилия, телефон, почта), вывести
 # имена тех, у кого не указана почта (нет ключа email или значение этого ключа -
 # пустая строка)
+user_dict: dict = {
+    1: {"f_name": 'Timyr', "l_name": 'Tishkevich', "tel_number": '375299301835', "email": 'timmi00@tut.by'},
+    99: {"f_name": 'Alex', "l_name": 'Coach', "tel_number": '375299999999', "email": 'coach@tut.by'},
+    6: {"f_name": 'Dima', "l_name": 'Gocha', "tel_number": '37527654387'},
+    3: {"f_name": 'Ivan', "l_name": 'Ivanov', "tel_number": '375299301835', 'email': 'python@python.py'},
+    19: {"f_name": 'Vasya', "l_name": 'Tishkevich', "tel_number": '375299301835', "email": ''},
+}
+for key, value in user_dict.items():
+    try:
+        if not value["email"]:
+            print(value['f_name'])
+    except KeyError as exc:
+        print(value['f_name'])
