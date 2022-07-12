09bba022182e6cca8c8a47526732511285422ace
Timmi00 <TIm200988>
2022-07-12 20:15:32 +0300

    solution

--- codewars/9.py
+++ codewars/9.py
@@ -1,12 +1,11 @@
 def order(sentence):
   sentence_list = sentence.split(' ')
-  number = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
   sentence_res = {}
   sentence_res_ = ''
   for i in sentence_list:
-    for n in number:
-      if n in i:
-        sentence_res[n] = i
+    for n in range(1, 10):
+      if str(n) in i:
+        sentence_res[str(n)] = i
   for key in range(1, len(sentence_res) + 1):
     sentence_res_ += sentence_res[str(key)]
     sentence_res_ += ' '
