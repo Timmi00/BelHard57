f2d007768eb9c7735859cf81ff82806f677f41a0
Timmi00 <TIm200988>
2022-07-12 09:59:58 +0300

    Add lite solution

--- Lesson_6/6.7.py
+++ Lesson_6/6.7.py
@@ -2,8 +2,6 @@
 # соседей, для крайних чисел одним из соседей является число с противоположной
 # стороны списка
 list_numbers: list[int] = [1, 2, 3, 4, 5, 6, 7, 8]
-# list_numbers.insert(0, list_numbers[len(list_numbers) - 1])
-# list_numbers.append(list_numbers[1])
 count = -1
 sum_result: list = []
 while count < len(list_numbers) - 1:
