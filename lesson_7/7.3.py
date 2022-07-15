4e3947f28d55670edabcdd7919c753fa7e58cd71
Timmi00 <TIm200988>
2022-07-15 17:35:34 +0300

    New solution

--- lesson_7/7.3.py
+++ lesson_7/7.3.py
@@ -1,13 +1,18 @@
 # На вход программе подаются два натуральных числа n и m. Напишите программу, которая создает матрицу размером n х m,
 # заполнив ее по спирали числами от 1 до n x m.
 # Спираль начинается в левом верхнем углу и закручивается по часовой стрелке.
-n, m, dx, dy, x, y = int(input()), int(input()), 0, 1, 0, 0
-matrix = [[0] * m for _ in range(n)]
-for i in range(1, n * m + 1):
-    matrix[x][y] = i
-    if matrix[(x + dx) % n][(y + dy) % m]:
-        dx, dy = dy, -dx
-    x += dx
-    y += dy
-for line in matrix:
-    print(*(f'{i:<9}' for i in line), sep='')
+n, m = int(input()), int(input())
+matrix = [None] * n * m
+x = (1, n, -1, -n)
+y = 0
+index = 0
+count = n
+for i in range(n * m):
+    matrix[index] = i + 1
+    if index == n * m - 1 or index == n - 1 or matrix[index + x[y]] or index == n * m - n:
+        y = (y + 1) & 3
+    index += x[y]
+while m:
+    print(matrix[n - count:n])
+    n += count
+    m -= 1
